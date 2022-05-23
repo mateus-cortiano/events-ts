f25540cef9069adb3e07a0e1c76c18b546f01cc5
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-23 16:04:17 -0300

    - implemented async emit correctly

--- lib/aevents.ts
+++ lib/aevents.ts
@@ -33,4 +33,26 @@ export class AsyncEventSystem<
 
     for (let sub of once_subs) this.remove(event, sub)
   }
+
+  async aemit<Event extends keyof Events>(
+    event: Event,
+    ...data: Parameters<Events[Event]>
+  ) {
+    let subs = this.subscribers.get(event)
+
+    if (subs === undefined) return
+
+    let promises: Promise<any>[] = []
+    let once_subs = []
+
+    for (let sub of subs) {
+      let promise = sub.call(...(data as Parameters<Events[Event]>[]))
+      if (sub.flags.once) once_subs.push(sub)
+      promises.push(promise)
+    }
+
+    await Promise.all(promises)
+
+    for (let sub of once_subs) this.remove(event, sub)
+  }
 }
