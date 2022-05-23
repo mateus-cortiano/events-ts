af84c812c3cec2578ffae13ec23ef82e5dab7a19
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-22 22:20:03 -0300

    - async events

---
+++ lib/aevents.ts
@@ -0,0 +1,33 @@
+/* aevents.ts */
+
+import { BaseEventsSystem, EventMap, Listener } from './base'
+
+// ---
+
+export interface DefaultAsyncEvents {
+  connect: (username: string) => Promise<void>
+  disconnect: (reason: string) => Promise<void>
+}
+
+export class AsyncEventSystem<
+  Events extends EventMap<Events> = DefaultAsyncEvents
+> extends BaseEventsSystem<Events> {
+  emit<Ev extends keyof Events>(event: Ev, ...data: Parameters<Events[Ev]>) {
+    let subs = this.subscribers.get(event)
+
+    if (subs === undefined) return
+
+    let promises: Promise<void>[] = []
+    let once_subs: Listener<Events[keyof Events]>[] = []
+
+    for (let sub of subs) {
+      let promise = sub.call(...(data as Events[Ev][]))
+      if (sub.flags.once) once_subs.push(sub)
+      promises.push(promise)
+    }
+
+    Promise.all(promises)
+
+    for (let sub of once_subs) this.remove(event, sub)
+  }
+}
