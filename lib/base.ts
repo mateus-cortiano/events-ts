6da184bb313170c6072c49dd9549dcee64d01795
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-22 23:30:12 -0300

    - added listener factory

--- lib/base.ts
+++ lib/base.ts
@@ -8,42 +8,50 @@ export interface ListenerFlags {
   once?: boolean
 }
 
-export interface Listener<T> {
+export interface IListener<T> {
   call: T
   flags: ListenerFlags
 }
 
+class Listener<T> implements IListener<T> {
+  constructor(public call: T, public flags: ListenerFlags = {}) {}
+}
+
+function default_listener<T>(call: T) {
+  return new Listener(call)
+}
+
+function once_listener<T>(call: T) {
+  return new Listener(call, { once: true })
+}
+
 export class BaseEventsSystem<Events extends EventMap<Events>> {
-  protected subscribers: Map<keyof Events, Listener<Events[keyof Events]>[]>
+  protected subscribers: Map<keyof Events, IListener<Events[keyof Events]>[]>
 
   constructor() {
     this.subscribers = new Map()
   }
 
   protected setdefault(event: keyof Events) {
-    let subs = this.subscribers.get(event)
-    if (subs === undefined) {
-      this.subscribers.set(event, [])
-      subs = this.subscribers.get(event)
-    }
-    return subs as Listener<Events[keyof Events]>[]
+    if (!this.subscribers.has(event)) this.subscribers.set(event, [])
+    return this.subscribers.get(event) as IListener<Events[keyof Events]>[]
   }
 
   on<Ev extends keyof Events>(event: Ev, callback: Events[Ev]) {
-    const listener = { call: callback, flags: {} }
+    const listener = default_listener(callback)
     this.setdefault(event).push(listener)
     return () => this.remove(event, listener)
   }
 
   once<Ev extends keyof Events>(event: Ev, callback: Events[Ev]) {
-    const listener = { call: callback, flags: { once: true } }
+    const listener = once_listener(callback)
     this.setdefault(event).push(listener)
     return () => this.remove(event, listener)
   }
 
   protected remove<Ev extends keyof Events>(
     event: Ev,
-    listener: Listener<Events[Ev]>
+    listener: IListener<Events[Ev]>
   ) {
     let subs = this.subscribers.get(event)
     if (subs === undefined) return
